cb0d9bc94609ad5790b2cdc2216f7b3e3d5bd00b
Antoine van Gelder <antoine@trigger.io>
2017-08-18 11:04:39 +0200

    Apply some lint to tests

--- module/tests/interactive.js
+++ module/tests/interactive.js
@@ -1,48 +1,50 @@
+/* globals module, $, forge, asyncTest, askQuestion, ok, start, runOnce */
+
 module("forge.parse");
 
 var channel = "channel"+Math.floor(Math.random()*10000);
 
 if (forge.is.mobile()) {
-	forge.parse.registerForNotifications(function () {
-		asyncTest("receive push", 1, function () {
-			forge.parse.push.subscribe(channel, function () {
-				$.ajax({
-					url: 'http://docker.trigger.io:1337/parse/push',
-					headers: {
-						"X-Parse-Application-Id": "45732VjSzMiN8HN90ztWcSeEl05T92XUrE70MJgI",
-						"X-Parse-MASTER-Key": "MASTER_KEY"
-					},
-					contentType: "application/json",
-					type: 'POST',
-					data: JSON.stringify({
-						channels: [channel],
-						data: { "alert": "†és† push "+((new Date()).toString()) }
-					}),
-					success: function () {
-						setTimeout(function () {
-							forge.partners.parse.push.unsubscribe(channel);
-						}, 60000);
-					},
-					error: function () {
-						setTimeout(function () {
-							forge.partners.parse.push.unsubscribe(channel);
-						}, 10000);
-					}
-				});
-			});
-			forge.event.messagePushed.addListener(runOnce(function (msg) {
-				askQuestion("Done.");
-				ok(true);
-				start();
-			}));
-			askQuestion("Wait a short while for a parse notification.", {
-				"No notification arrived": function () {
-					ok(false);
-					start();
-				}
-			});
-		});
-	}, function (error) {
-		forge.logging.log("Failed to register for remote notifications: " + JSON.stringify(error));
-	});
+    forge.parse.registerForNotifications(function () {
+        asyncTest("receive push", 1, function () {
+            forge.parse.push.subscribe(channel, function () {
+                $.ajax({
+                    url: 'http://docker.trigger.io:1337/parse/push',
+                    headers: {
+                        "X-Parse-Application-Id": "45732VjSzMiN8HN90ztWcSeEl05T92XUrE70MJgI",
+                        "X-Parse-MASTER-Key": "MASTER_KEY"
+                    },
+                    contentType: "application/json",
+                    type: 'POST',
+                    data: JSON.stringify({
+                        channels: [channel],
+                        data: { "alert": "†és† push "+((new Date()).toString()) }
+                    }),
+                    success: function () {
+                        setTimeout(function () {
+                            forge.partners.parse.push.unsubscribe(channel);
+                        }, 60000);
+                    },
+                    error: function () {
+                        setTimeout(function () {
+                            forge.partners.parse.push.unsubscribe(channel);
+                        }, 10000);
+                    }
+                });
+            });
+            forge.event.messagePushed.addListener(runOnce(function () {
+                askQuestion("Done.");
+                ok(true);
+                start();
+            }));
+            askQuestion("Wait a short while for a parse notification.", {
+                "No notification arrived": function () {
+                    ok(false);
+                    start();
+                }
+            });
+        });
+    }, function (error) {
+        forge.logging.log("Failed to register for remote notifications: " + JSON.stringify(error));
+    });
 }
