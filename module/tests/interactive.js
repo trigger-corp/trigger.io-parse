970020d57e0e8c1a238356629f54820772d6eea2
Antoine van Gelder <antoine@trigger.io>
2017-09-26 15:26:24 +0200

    Update tests to handle option: updateNotifications

--- module/tests/interactive.js
+++ module/tests/interactive.js
@@ -2,8 +2,13 @@
 
 module("forge.parse");
 
-var channel = "channel"+Math.floor(Math.random()*10000);
+var channel = "channel" + Math.floor(Math.random()*10000);
 
+var parse_endpoint = "http://docker.trigger.io:1337/parse/push";
+var parse_headers = {
+    "X-Parse-Application-Id": "45732VjSzMiN8HN90ztWcSeEl05T92XUrE70MJgI",
+    "X-Parse-MASTER-Key": "MASTER_KEY"
+};
 
 asyncTest("Delay registration", 1, function () {
     forge.parse.registerForNotifications(function () {
@@ -25,31 +30,35 @@ asyncTest("Delay registration", 1, function () {
 });
 
 
-asyncTest("receive push", 1, function () {
-    forge.parse.push.subscribe(channel, function () {
-        $.ajax({
-            url: 'http://docker.trigger.io:1337/parse/push',
-            headers: {
-                "X-Parse-Application-Id": "45732VjSzMiN8HN90ztWcSeEl05T92XUrE70MJgI",
-                "X-Parse-MASTER-Key": "MASTER_KEY"
-            },
-            contentType: "application/json",
-            type: 'POST',
-            data: JSON.stringify({
-                channels: [channel],
-                data: { "alert": "†és† push "+((new Date()).toString()) }
-            }),
-            success: function () {
-                setTimeout(function () {
-                    forge.partners.parse.push.unsubscribe(channel);
-                }, 60000);
-            },
-            error: function () {
-                setTimeout(function () {
-                    forge.partners.parse.push.unsubscribe(channel);
-                }, 10000);
+function send_push(channel, message) {
+    $.ajax({
+        url: parse_endpoint,
+        headers: parse_headers,
+        contentType: "application/json",
+        type: "POST",
+        data: JSON.stringify({
+            channels: [channel],
+            data: {
+                "alert": message + ": " + ((new Date()).toString())
             }
-        });
+        }),
+        success: function () {
+            setTimeout(function () {
+                forge.partners.parse.push.unsubscribe(channel);
+            }, 60000);
+        },
+        error: function () {
+            setTimeout(function () {
+                forge.partners.parse.push.unsubscribe(channel);
+            }, 60000);
+        }
+    });
+}
+
+
+/*asyncTest("receive push", 1, function () {
+    forge.parse.push.subscribe(channel, function () {
+        send_push(channel, "†és† push");
     });
     forge.event.messagePushed.addListener(runOnce(function () {
         askQuestion("Done.");
@@ -60,6 +69,30 @@ asyncTest("receive push", 1, function () {
         "No notification arrived": function () {
             ok(false);
             start();
+        }
+    });
+});*/
+
+
+asyncTest("receive push in background", 1, function () {
+    askQuestion("Click OK, put your app in the background and click on the notification when it arrives", {
+        Ok: function () {
+            forge.event.messagePushed.addListener(runOnce(function () {
+                ok(true);
+                start();
+            }));
+            forge.parse.push.subscribe(channel, function () {
+                forge.event.appPaused.addListener(function () {
+                    forge.logging.log("Sending push");
+                    send_push(channel, "update notifications push");
+                });
+            });
+            askQuestion("Wait a short while for a parse notification.", {
+                "No notification arrived": function () {
+                    ok(false);
+                    start();
+                }
+            });
         }
     });
 });
